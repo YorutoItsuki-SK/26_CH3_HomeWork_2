fd2f7c9edfbb9630d7ac63bc82ee0647cf02f111
이승기 <thouxcv@gmail.com>
2026-08-11 17:51:31 +0900

    추가, 부유 객체, 기본 함수 추가

--- Source/CH_Homework2/Private/Character/FlyingOne.cpp
+++ Source/CH_Homework2/Private/Character/FlyingOne.cpp
@@ -1,34 +1,143 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿#include "Character/FlyingOne.h"
+#include "Components/BoxComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "EnhancedInputComponent.h"
+#include "Controller/FlyingController.h"
 
-
-#include "Character/FlyingOne.h"
-
-// Sets default values
 AFlyingOne::AFlyingOne()
 {
- 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	USceneComponent* SceneComponent = GetRootComponent();
+	BoxComponent = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxComponent"));
+	BoxComponent->SetSimulatePhysics(false);
+	BoxComponent->SetupAttachment(SceneComponent);
+
+	SkeletalMeshComponent = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("SkeletalMesh"));
+	//SkeletalMeshComponent->SetSimulatePhysics(false);
+	SkeletalMeshComponent->SetupAttachment(BoxComponent);
+
+	SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArmComponent->TargetArmLength = 300.f;
+	SpringArmComponent->bUsePawnControlRotation = true;
+	SpringArmComponent->SetupAttachment(BoxComponent);
+
+	CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	CameraComponent->bUsePawnControlRotation = false;
+	CameraComponent->SetupAttachment(SpringArmComponent, USpringArmComponent::SocketName);
+
+	SpeedMoveMax = 600.f;
+	//AccelMove = 60.f;
+	SpeedRotateMax = 30.f;
+	//AccelRotate = 60.f;
+	SpeedFlyMax = 600.f;
+	//AccelFly = 60.f;
 
 }
 
-// Called when the game starts or when spawned
 void AFlyingOne::BeginPlay()
 {
 	Super::BeginPlay();
 	
 }
 
-// Called every frame
 void AFlyingOne::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
+	CurrentDeltaTime = DeltaTime;
 }
 
-// Called to bind functionality to input
 void AFlyingOne::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	UEnhancedInputComponent* EnhacedInput = Cast<UEnhancedInputComponent>(PlayerInputComponent);
+	if (!EnhacedInput) {
+		return;
+	}
+	
+	AFlyingController* FlyingController = Cast<AFlyingController>(GetController());
+	if (!FlyingController) {
+		return;
+	}
+
+	if (FlyingController->MoveAction) {
+		EnhacedInput->BindAction(
+			FlyingController->MoveAction,
+			ETriggerEvent::Triggered,
+			this,
+			AFlyingOne::Move
+		);
+	}
+	if (FlyingController->LookAction) {
+		EnhacedInput->BindAction(
+			FlyingController->LookAction,
+			ETriggerEvent::Triggered,
+			this,
+			AFlyingOne::Look
+		);
+	}
+	if (FlyingController->FloatAction) {
+		EnhacedInput->BindAction(
+			FlyingController->FloatAction,
+			ETriggerEvent::Triggered,
+			this,
+			AFlyingOne::Float
+		);
+	}
+	if (FlyingController->SinkAction) {
+		EnhacedInput->BindAction(
+			FlyingController->SinkAction,
+			ETriggerEvent::Triggered,
+			this,
+			AFlyingOne::Sink
+		);
+	}
+	if (FlyingController->RollLeftAction) {
+		EnhacedInput->BindAction(
+			FlyingController->RollLeftAction,
+			ETriggerEvent::Triggered,
+			this,
+			AFlyingOne::RollLeft
+		);
+	}
+	if (FlyingController->RollRightAction) {
+		EnhacedInput->BindAction(
+			FlyingController->RollRightAction,
+			ETriggerEvent::Triggered,
+			this,
+			AFlyingOne::RollRight
+		);
+	}
+}
+
+void AFlyingOne::Move(const FInputActionValue& Value)
+{
+	FVector2D MoveInput = Value.Get<FVector2D>();
+	if (!FMath::IsNearlyZero(MoveInput.X)) {
+		FVector FowardVector = GetActorForwardVector();
+		
+	}
+}
+
+void AFlyingOne::Look(const FInputActionValue& Value)
+{
+}
+
+void AFlyingOne::Float(const FInputActionValue& Value)
+{
+}
+
+void AFlyingOne::Sink(const FInputActionValue& Value)
+{
+}
+
+void AFlyingOne::RollLeft(const FInputActionValue& Value)
+{
+}
+
+void AFlyingOne::RollRight(const FInputActionValue& Value)
+{
 }
 
--- Source/CH_Homework2/Public/Character/FlyingOne.h
+++ Source/CH_Homework2/Public/Character/FlyingOne.h
@@ -1,10 +1,14 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#pragma once
+﻿#pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
 #include "FlyingOne.generated.h"
+
+class USpringArmComponent;
+class UCameraComponent;
+class UBoxComponent;
+class USkeletalMeshComponent;
+struct FInputActionValue;
 
 UCLASS()
 class CH_HOMEWORK2_API AFlyingOne : public APawn
@@ -12,18 +16,52 @@ class CH_HOMEWORK2_API AFlyingOne : public APawn
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this pawn's properties
 	AFlyingOne();
 
 protected:
-	// Called when the game starts or when spawned
+	float CurrentDeltaTime = 0;
+
+	/*float AccelMove;
+	float AccelRotate;
+	float AccelFly;*/
+	float Gravity = 980.f;
+
+	float SpeedMoveMax;
+	float SpeedRotateMax;
+	float SpeedFlyMax;
+
+	float SpeedMove = 0;
+	float SpeedRotate = 0;
+	float SpeedFly = 0;
+
 	virtual void BeginPlay() override;
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Visual")
+	TObjectPtr<UBoxComponent> BoxComponent;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Visual")
+	TObjectPtr<USkeletalMeshComponent> SkeletalMeshComponent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Camera")
+	TObjectPtr<USpringArmComponent> SpringArmComponent;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Camera")
+	TObjectPtr<UCameraComponent> CameraComponent;
+
 public:	
-	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UFUNCTION();
+	void Move(const FInputActionValue& Value);
+	UFUNCTION();
+	void Look(const FInputActionValue& Value);
+	UFUNCTION();
+	void Float(const FInputActionValue& Value);
+	UFUNCTION();
+	void Sink(const FInputActionValue& Value);
+	UFUNCTION();
+	void RollLeft(const FInputActionValue& Value);
+	UFUNCTION();
+	void RollRight(const FInputActionValue& Value);
 
 };
